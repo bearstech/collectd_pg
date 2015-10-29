f4f1c2c30ad2088e9e89fe0840b4271fc2059628
Mathieu Lecarme <mlecarme@bearstech.com>
2015-10-29 18:37:08 +0100

    More collectd.

--- pg.py
+++ pg.py
@@ -1,17 +1,72 @@
-import psycopg2 as pg
+from urlparse import urlparse
+import psycopg2
 
 
-COLUMNS = map(lambda a: a.strip(), "datid |    datname    | numbackends | xact_commit | xact_rollback | blks_read | blks_hit  | tup_returned | tup_fetched | tup_inserted | tup_updated | tup_deleted | conflicts | temp_files | temp_bytes  | deadlocks | blk_read_time | blk_write_time |          stats_reset".split("|"))
+COLUMNS = map(lambda a: a.strip(), "datid |    datname    | numbackends \
+| xact_commit | xact_rollback | blks_read | blks_hit  | tup_returned \
+| tup_fetched | tup_inserted | tup_updated | tup_deleted | conflicts \
+| temp_files | temp_bytes  | deadlocks | blk_read_time | blk_write_time \
+|          stats_reset".split("|"))
 
 
-def stats(url):
-    conn = pg.connect(url)
+def stats(uri):
+    conn = psycopg2.connect(uri)
     cur = conn.cursor()
     cur.execute("""SELECT *
                 FROM pg_stat_database
                 WHERE datname NOT LIKE 'template%' ORDER BY datname;""")
     for line in cur:
         yield dict(zip(COLUMNS, line))
+
+try:
+    import collectd
+except ImportError:
+    # we're not running inside collectd
+    # it's ok
+    pass
+else:
+
+    def logger(t, msg):
+        if t == 'err':
+            collectd.error('%s: %s' % (NAME, msg))
+        elif t == 'warn':
+            collectd.warning('%s: %s' % (NAME, msg))
+        elif t == 'info':
+            collectd.info('%s: %s' % (NAME, msg))
+        else:
+            collectd.notice('%s: %s' % (NAME, msg))
+
+    NAME = "pg"
+    uris = []
+
+    def config_callback(conf):
+        global uris
+        for node in conf.children:
+            if node.key == "uri":
+                uris += node.values
+            else:
+                logger('warn', "Unknown config %s" % node.key)
+
+    def read_callback():
+        global uris
+        for uri in uris:
+            server = urlparse(uri).hostname
+            for stat in stats(uri):
+                for v in ['xact_commit', 'xact_rollback', 'blks_read',
+                          'blks_hit', 'tup_returned', 'tup_fetched',
+                          'tup_inserted', 'tup_updated', 'tup_deleted',
+                          'conflicts', 'temp_files', 'temp_bytes', 'deadlocks',
+                          'blk_read_time', 'blk_write_time']:
+                    val = collectd.Values(plugin=NAME, type="absolute")
+                    val.host = server
+                    val.plugin_instance = stat['datname']
+                    val.values = [stat[v]]
+                    val.type = "absolute"
+                    val.type_instance = v
+                    val.dispatch()
+
+    collectd.register_config(config_callback)
+    collectd.register_read(read_callback)
 
 
 if __name__ == "__main__":
